e30c000e83d1d8bb5e87c0cc806ce5d34b367f0f
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2017-12-11 18:21:19 +0000

    Added complex passing to sge command

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-alias qsub=echo
-qsub "Starting tests"
+PATH="$(pwd)/bin:$PATH"
+export PATH
+echo "Starting tests"
+
+# Exit states
+INVALID_SYNTAX="64"
+NO_CUDA="65"
+NO_CUDA_TYPE="66"
+MISS_CONFIGURED="67"
+ARG_MISSING_VALUE="68"
+MISSING_FILE="69"
+NO_CUDA_VERSION="70"
+NO_EXECUTABLE="71"
+BAD_QUEUE="72"
 
 chmod -x test_script.sh
-result=`../fsl_sub ./test_script.sh 2>&1`
+result=$(bash -x ../fsl_sub ./test_script.sh)
 status=$?
-if [ $status -ne 126 ]; then
+if [ $status -eq $NO_EXECUTABLE ]; then
     echo "Test 1 failed - status is $status, should be 126"
     echo $result
 else
@@ -15,9 +27,9 @@ else
 fi
 
 chmod +x test_script.sh
-result=`../fsl_sub test_script.sh 2>&1`
+result=$(../fsl_sub test_script.sh 2>&1)
 status=$?
-if [ $status -ne -1 ]; then
+if [ $status -eq $NO_EXECUTABLE ]; then
     echo "Test 2 failed"
 else
     if [ "$result" != "The command you have requested cannot be found or is not executable" ]; then
@@ -26,9 +38,9 @@ else
 fi
 
 chmod -x test_script.sh
-result=`../fsl_sub -t test_parallel 2>&1`
+result=$(../fsl_sub -t test_parallel 2>&1)
 status=$?
-if [ $status -ne -1 ]; then
+if [ $status -eq $NO_EXECUTABLE ]; then
     echo "Test 3 failed"
 else
     if [ "$result" != "The command test_script.sj in the task file test_parallel, line 1 cannot be found or is not executable" ]; then
@@ -37,9 +49,9 @@ else
 fi
 
 chmod +x test_script.sh
-result=`../fsl_sub -t test_parallel_fail_1 2>&1`
+result=$(../fsl_sub -t test_parallel_fail_1 2>&1)
 status=$?
-if [ $status -ne -1 ]; then
+if [ $status -eq $NO_EXECUTABLE ]; then
     echo "Test 4 failed"
 else
     if [ "$result" != "The command test_script.sj in the task file test_parallel, line 2 cannot be found or is not executable" ]; then
@@ -48,9 +60,9 @@ else
 fi
 
 chmod +x test_script.sh
-result=`../fsl_sub -t test_parallel_fail_2 2>&1`
+result=$(../fsl_sub -t test_parallel_fail_2 2>&1)
 status=$?
-if [ $status -ne -1 ]; then
+if [ $status -eq $NO_EXECUTABLE ]; then
     echo "Test 5 failed"
 else
     if [ "$result" != "The command test_script.sj in the task file test_parallel, line 2 cannot be found or is not executable" ]; then
