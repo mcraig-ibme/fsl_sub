a81172af1c0a0b9e18cf5e6f170471b2aa52f8af
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2018-02-05 11:18:09 +0000

    Re-order functions into more logical groups

--- fsl_sub.py
+++ fsl_sub.py
@@ -54,77 +54,6 @@ class UnrecognisedModule(Exception):
 
 
 @lru_cache
-def list_coprocessors(config):
-    '''Return a list of coprocessors found in the queue definitions'''
-
-    avail_cops = []
-
-    for q in config['queues']:
-        try:
-            avail_cops.extend(q['copros'].keys())
-        except KeyError:
-            pass
-
-    return avail_cops
-
-
-@lru_cache
-def max_coprocessors(config, coprocessor):
-    '''Return the maximum number of coprocessors per node from the
-    queue definitions'''
-
-    num_cops = 0
-
-    for q in config['queues']:
-        if 'copros' in q:
-            try:
-                num_cops = max(
-                    num_cops,
-                    q['copros'][coprocessor]['max_quantity'])
-            except KeyError:
-                pass
-
-    return num_cops
-
-
-@lru_cache
-def copro_classes(config, coprocessor):
-    '''Return whether a coprocessor supports multiple classes of hardware.
-    Classes are sorted by capability'''
-    classes = defaultdict(lambda: 1)
-    copro_opts = config['copro_opts'][coprocessor]
-    for q in config['queues']:
-        if 'copros' in q:
-            try:
-                for c in q['copros'][coprocessor]['classes']:
-                    classes[c] = copro_opts[c]['capability']
-            except KeyError:
-                continue
-    if not classes:
-        return None
-    return sorted(classes.keys(), key=classes.get)
-
-
-def coprocessor_toolkits(config, coprocessor):
-    '''Return list of coprocessor toolkit versions.'''
-    if coprocessor not in config['copro_opts']:
-        raise BadConfiguration(
-            "Coprocessor {} not configured".format(coprocessor))
-    # Check that we have queues configured for this coproceesor
-    if not all([q for q in config['queues'] if (
-            'copros' in q and coprocessor in q['copros'])]):
-        raise BadConfiguration(
-            "Coprocessor {} not available in any queues".format(
-                coprocessor
-            )
-        )
-    copro_conf = config['copro_opts'][coprocessor]
-    if not copro_conf['uses_modules']:
-        return None
-    return get_modules(copro_conf['module_parent']).sort
-
-
-@lru_cache
 def parallel_envs(queues):
     '''Return the list of configured parallel environments
     in the supplied queue definition dict'''
@@ -245,6 +174,100 @@ def module_string(module_parent, module_version):
         return "/".join((module_parent, module_version))
     else:
         return module_parent
+
+
+@lru_cache
+def list_coprocessors(config):
+    '''Return a list of coprocessors found in the queue definitions'''
+
+    avail_cops = []
+
+    for q in config['queues']:
+        try:
+            avail_cops.extend(q['copros'].keys())
+        except KeyError:
+            pass
+
+    return avail_cops
+
+
+@lru_cache
+def max_coprocessors(config, coprocessor):
+    '''Return the maximum number of coprocessors per node from the
+    queue definitions'''
+
+    num_cops = 0
+
+    for q in config['queues']:
+        if 'copros' in q:
+            try:
+                num_cops = max(
+                    num_cops,
+                    q['copros'][coprocessor]['max_quantity'])
+            except KeyError:
+                pass
+
+    return num_cops
+
+
+@lru_cache
+def copro_classes(config, coprocessor):
+    '''Return whether a coprocessor supports multiple classes of hardware.
+    Classes are sorted by capability'''
+    classes = defaultdict(lambda: 1)
+    copro_opts = config['copro_opts'][coprocessor]
+    for q in config['queues']:
+        if 'copros' in q:
+            try:
+                for c in q['copros'][coprocessor]['classes']:
+                    classes[c] = copro_opts[c]['capability']
+            except KeyError:
+                continue
+    if not classes:
+        return None
+    return sorted(classes.keys(), key=classes.get)
+
+
+def coprocessor_toolkits(config, coprocessor):
+    '''Return list of coprocessor toolkit versions.'''
+    if coprocessor not in config['copro_opts']:
+        raise BadConfiguration(
+            "Coprocessor {} not configured".format(coprocessor))
+    # Check that we have queues configured for this coproceesor
+    if not all([q for q in config['queues'] if (
+            'copros' in q and coprocessor in q['copros'])]):
+        raise BadConfiguration(
+            "Coprocessor {} not available in any queues".format(
+                coprocessor
+            )
+        )
+    copro_conf = config['copro_opts'][coprocessor]
+    if not copro_conf['uses_modules']:
+        return None
+    return get_modules(copro_conf['module_parent']).sort
+
+def coproc_class(coproc_class, coproc_classes):
+    try:
+        for c, i in enumerate(coproc_classes):
+            if c['shortcut'] == coproc_class:
+                break
+    except KeyError:
+        raise BadConfiguration(
+            "Co-processor class {} not configured".format(coproc_class),
+            file=sys.stderr)
+    return coproc_classes[:i]
+
+
+def coproc_load_module(coproc, module_version):
+    if coproc['uses_modules']:
+        modules_avail = get_modules(coproc['module_parent'])
+        if modules_avail:
+            if module_version not in modules_avail:
+                raise UnrecognisedModule(module_version)
+            else:
+                load_module("/".join(
+                    (coproc['module_parent'], module_version)))
+
 
 
 def build_parser(config):
@@ -802,27 +825,6 @@ def system(
             check=check, universal_newlines=True)
 
 
-def coproc_class(coproc_class, coproc_classes):
-    try:
-        for c, i in enumerate(coproc_classes):
-            if c['shortcut'] == coproc_class:
-                break
-    except KeyError:
-        raise BadConfiguration(
-            "Co-processor class {} not configured".format(coproc_class),
-            file=sys.stderr)
-    return coproc_classes[:i]
-
-
-def coproc_load_module(coproc, module_version):
-    if coproc['uses_modules']:
-        modules_avail = get_modules(coproc['module_parent'])
-        if modules_avail:
-            if module_version not in modules_avail:
-                raise UnrecognisedModule(module_version)
-            else:
-                load_module("/".join(
-                    (coproc['module_parent'], module_version)))
 
 
 def getq_and_slots(
