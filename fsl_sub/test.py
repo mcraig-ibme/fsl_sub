1993a452504579cf2f12204f7a9fb70d8bb4477f
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2018-02-22 17:15:36 +0000

    move test_fsl_sub_None to module package

---
+++ fsl_sub/test.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+import unittest
+import yaml
+
+
+class TestParallelEnvs(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.config = yaml.load('''
+method: SGE
+thread_control:
+  - OMP_THREADS
+  - MKL_NUM_THREADS
+method_opts:
+  None:
+    parallel_envs: []
+    same_node_pes: []
+    large_job_split_pe: []
+    mail_support: False
+    map_ram: False
+    job_priorities: False
+    parallel_holds: False
+    parallel_limit: False
+    architecture: False
+    job_resources: False
+  SGE:
+    parallel_envs:
+      - shmem
+    same_node_pes:
+      - shmem
+    large_job_split_pe: shmem
+queues:
+  short.q:
+    time: 1440
+    max_size: 160
+    slot_size: 4
+    max_slots: 16
+    map_ram: true
+    parallel_envs:
+      - shmem
+    priority: 3
+    group: 1
+    default: true
+  long.q:
+    time: 10080
+    max_size: 160
+    slot_size: 4
+    max_slots: 16
+    map_ram: true
+    parallel_envs:
+      - shmem
+    priority: 3
+    group: 2
+''')
+
+    def test_parallel_envs(self):
+        with self.subTest('Test 1'):
+            self.assertListEqual(
+                fsl_sub.parallel_envs(self.config['queues']),
+                ['shmem', ]
+            )
+        with self.subTest('Test 2'):
+            self.config['queues']['long.q']['parallel_envs'] = ['make', ]
+            self.assertCountEqual(
+                fsl_sub.parallel_envs(self.config['queues']),
+                ['shmem', 'make', ]
+            )
+        with self.subTest('Test 3'):
+            self.config['queues']['long.q']['parallel_envs'] = [
+                'make', 'shmem', ]
+            self.assertCountEqual(
+                fsl_sub.parallel_envs(self.config['queues']),
+                ['shmem', 'make', ]
+            )
+
+    @patch('fsl_sub.parallel_envs')
+    def test_process_pe_def(self, mock_parallel_envs):
+        mock_parallel_envs.return_value = ['openmp', ]
+        queues = self.config['queues']
+        with self.subTest('Success'):
+            self.assertEqual(
+                {'name': 'openmp', 'slots': 2},
+                fsl_sub.process_pe_def(
+                    'openmp,2',
+                    queues
+                )
+            )
+        with self.subTest('Bad input'):
+            self.assertRaises(
+                fsl_sub.ArgumentError,
+                fsl_sub.process_pe_def,
+                'openmp.2',
+                queues
+            )
+        with self.subTest("No PE"):
+            self.assertRaises(
+                fsl_sub.ArgumentError,
+                fsl_sub.process_pe_def,
+                'mpi,2',
+                queues
+            )
+        with self.subTest("No PE"):
+            self.assertRaises(
+                fsl_sub.ArgumentError,
+                fsl_sub.process_pe_def,
+                'mpi,A',
+                queues
+            )
+        with self.subTest("No PE"):
+            self.assertRaises(
+                fsl_sub.ArgumentError,
+                fsl_sub.process_pe_def,
+                'mpi,2.2',
+                queues
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
