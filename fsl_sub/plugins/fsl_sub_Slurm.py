69f05ca74c463886ba6dc65e36638824537c0646
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2018-04-29 12:18:56 +0100

    and again

--- fsl_sub/plugins/fsl_sub_Slurm.py
+++ fsl_sub/plugins/fsl_sub_Slurm.py
@@ -135,7 +135,7 @@ def submit(
     usescript - queue config is defined in script
     '''
 
-    logger = logging.getFLogger(__name__)
+    logger = logging.getLogger(__name__)
 
     if command is None:
         raise BadSubmission(
