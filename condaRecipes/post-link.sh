800a981f072bdeaabca39f87feb82c84ed5bc244
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2018-05-22 13:38:56 +0100

    Add conda build files

---
+++ condaRecipes/post-link.sh
@@ -0,0 +1,3 @@
+if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh ]; then
+    $FSLDIR/etc/fslconf/requestFSLpythonLink.sh fsl_sub fsl_sub_config
+fi
---
+++ condaRecipes/pre-unlink.sh
@@ -0,0 +1,3 @@
+if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonUnlink.sh ]; then
+    $FSLDIR/etc/fslconf/requestFSLpythonUnlink.sh fsl_sub fsl_sub_config
+fi
