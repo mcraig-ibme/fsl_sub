aa1e98007d442b4b0b2111886898d726a3f840b1
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2019-02-04 16:08:33 +0000

    Make post/pre unlink only run if FSLDIR set

--- condaRecipes/post-link.sh
+++ condaRecipes/post-link.sh
@@ -1,3 +1,6 @@
-if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh ]; then
-    $FSLDIR/etc/fslconf/requestFSLpythonLink.sh fsl_sub fsl_sub_config
+#!/bin/bash
+if [ -n "${FSLDIR}" ]; then
+    if [ -e "${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh" ]; then
+        "$FSLDIR/etc/fslconf/requestFSLpythonLink.sh" fsl_sub fsl_sub_config
+    fi
 fi
