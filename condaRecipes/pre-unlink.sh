ad2f7fa7265750f01fc78abea4dab8cbf8926baf
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2020-11-09 15:29:38 +0000

    Add additional tools fsl FSLDIR/bin install

--- condaRecipes/pre-unlink.sh
+++ condaRecipes/pre-unlink.sh
@@ -2,7 +2,7 @@
 if [ -n "${FSLDIR}" ]; then
     if [ -w "${FSLDIR}/bin" ]; then
         if [ -e "${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh" ]; then
-            "$FSLDIR/etc/fslconf/requestFSLpythonLink.sh" fsl_sub fsl_sub_config
+            "$FSLDIR/etc/fslconf/requestFSLpythonLink.sh" fsl_sub fsl_sub_config fsl_sub_plugin fsl_sub_report fsl_sub_update
         fi
     fi
 fi
