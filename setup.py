507ec7777fc4d40308ba24c836729f822d527df5
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2020-11-16 18:03:27 +0000

    Poor copy-paste from plugin example!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open('fsl_sub/version.py', mode='r') as vf:
     vl = vf.read().strip()
 
-PLUGIN_VERSION = vl.split(' = ')[1].strip("'")
+VERSION = vl.split(' = ')[1].strip("'")
 
 setup(
     name='fsl_sub',
